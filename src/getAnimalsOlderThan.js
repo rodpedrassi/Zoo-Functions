2c166fdd4b417ae0d5d23fba543c8935b8eb2b90
rodpedrassi <rodpedrassi@gmail.com>
2022-06-29 17:47:38 -0300

    requisito 2 feito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species
+    .find((specie) => specie.name === animal)
+    .residents.every((specie) => specie.age > age);
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
