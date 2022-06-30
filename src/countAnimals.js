f59e8cd84aee9cae7a72bf8d0815eeca47c53638
rodpedrassi <rodpedrassi@gmail.com>
2022-06-30 14:27:18 -0300

    Requisito 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allAnimals = {};
+    species.forEach((specie) => {
+      allAnimals[specie.name] = specie.residents.length;
+    });
+    return allAnimals;
+  }
+  const specieByName = species.find((specie) => specie.name === animal.specie);
+  if (!animal.sex) {
+    return specieByName.residents.length;
+  }
+  return specieByName.residents.filter((specie) => specie.sex === animal.sex).length;
 }
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
